2e77c4d2a43be036b99526b95d295d8f6b2d2d85
Nick Taras <driverbulba@gmail.com>
2015-07-07 22:48:56 +0300

    Changed recognizer input to AudioData.msg

--- pandora_voice_recognition/nodes/recognizer.py
+++ pandora_voice_recognition/nodes/recognizer.py
@@ -2,11 +2,14 @@
 
 import sys, os
 from pocketsphinx import *
-import alsaaudio, numpy
+import alsaaudio
+import numpy as np
 import roslib; roslib.load_manifest('pandora_voice_recognition')
 import rospy
 from std_msgs.msg import String
 import commands
+from pandora_audio_msgs.msg import AudioData
+import struct
 
 class recognizer(object):
 	def __init__(self):
@@ -19,7 +22,21 @@ class recognizer(object):
 		self.keyphrase_dir = rospy.get_param("~keyphrase_dir")  #full path and filename
 
 		self.pub = rospy.Publisher('/recognizer/output',String)
+		rospy.Subscriber('/pandora_audio/kinect_audio_capture_stream', AudioData, self.callback, queue_size=1)
 
+		self.recognizer_channels = 1
+
+		#prosoxh! Afta einai gia monitor
+		self.pcm = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
+		self.pcm.setchannels(1)
+		self.pcm.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+		self.pcm.setrate(16000)
+		self.pcm.setperiodsize(1024)
+		#########
+
+		##configure
+		self.gain = 1
+		###
 
 		self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, 'default')
 		self.config = Decoder.default_config()
@@ -33,26 +50,41 @@ class recognizer(object):
 		self.inp.setrate(16000)
 		self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 		self.inp.setperiodsize(1024)
-		self.recognize()
+
+		self.decoder = Decoder(self.config)
+		self.decoder.start_utt()
+		rospy.spin()
 
 		# Process audio chunk by chunk. On keyword detected perform action and restart search
-	def recognize(self):
-		decoder = Decoder(self.config)
-		decoder.start_utt()
-		while True:
-		    length, buf = self.inp.read()  #length is not used
-		    decoder.process_raw(buf, False, False)
-		    if decoder.hyp() != None:
-						msg = String()
-						msg.data =decoder.hyp().hypstr
-						self.pub.publish(msg)
-						print "Detected victim word: ", decoder.hyp().hypstr
-						decoder.end_utt()
-						decoder.start_utt()
+	def callback(self,data):
+		#length, buf = self.inp.read()  #length is not used
+		c = []
+		c1 = np.array(data.channel1)
+		c2 = np.array(data.channel2)
+		c3 = np.array(data.channel3)
+		c4 = np.array(data.channel4)
+		for k in range(0, len(c1)):
+			#temp = self.gain*(c1[k]+c2[k]+c3[k]+c4[k])/4
+			temp = self.gain*c2[k]
+			c.append(temp)
+
+		buf = struct.pack("<" + str(1024) + 'h', *c)
+
+		self.pcm.write(buf)
+
+		self.decoder.process_raw(buf, False, False)
+		if self.decoder.hyp() != None:
+			msg = String()
+			msg.data = self.decoder.hyp().hypstr+" "+str(self.decoder.hyp().prob)
+			self.pub.publish(msg)
+			#rospy.loginfo("Detected victim word: ", self.decoder.hyp().hypstr)
+
+			self.decoder.end_utt()
+			self.decoder.start_utt()
 
 
 if __name__ == "__main__":
-	# try:
+	try:
 	  start = recognizer()
-	#except rospy.ROSInterruptException:
-	#	pass
+	except rospy.ROSInterruptException:
+		pass
